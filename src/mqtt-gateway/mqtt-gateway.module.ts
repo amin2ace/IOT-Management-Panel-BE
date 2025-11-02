4d60609793b57e36c7dfaf9029f959bb1ee3bfd3
Amin Vahdani <amin.vahdani@payever.org>
2025-11-02 11:06:11 +0330

    rename

--- src/mqtt-gateway/mqtt-gateway.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MqttGatewayService } from './mqtt-gateway.service';
-import { MqttGatewayGateway } from './mqtt-gateway.gateway';
-
-@Module({
-  providers: [MqttGatewayGateway, MqttGatewayService],
-})
-export class MqttGatewayModule {}
