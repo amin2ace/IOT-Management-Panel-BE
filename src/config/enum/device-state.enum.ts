1c109657d92c83fd558b6d3a3c9bb235875109d6
Amin Vahdani <amin.vahdani@payever.org>
2025-11-03 09:49:32 +0330

    add specific enums

--- src/config/enum/device-state.enum.ts
+++ src/config/enum/device-state.enum.ts
@@ -1,6 +1,7 @@
-export enum DeviceState {
-  UNASSIGNED = 'unassigned',
-  ASSIGNED = 'assigned',
-  ACTIVE = 'active',
-  ERROR = 'error',
+export enum ProvisionState {
+  DISCOVERED = 'discovered', // received capabilities but not assigned
+  UNASSIGEND = 'unassigned', // was assigned but now unassigned
+  ASSIGNED = 'assigned', // assigned a type, config sent
+  ACTIVE = 'active', // sending valid data
+  ERROR = 'error', // data invalid or offline
 }
---
+++ src/config/enum/protocol.enum.ts
@@ -0,0 +1,6 @@
+export enum Protocol {
+  MQTT = 'MQTT',
+  HTTP = 'HTTP',
+  MODBUS_TCP = 'MODBUS_TCP',
+  MODBUS_RTU = 'MODBUS_RTU',
+}
