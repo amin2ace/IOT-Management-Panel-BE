f1cd2a6ad2af8a1bf2a968644aa7b962f1eda35d
Amin Vahdani <amin.vahdani@payever.org>
2025-11-03 13:56:17 +0330

    rename

---
+++ src/config/enum/device-capabilities.enum.ts
@@ -0,0 +1,12 @@
+export enum DeviceCapabilities {
+  TEMPERATURE = 'temperature',
+  HUMIDITY = 'humidity',
+  PRESSURE = 'pressure',
+  LIGHT = 'light',
+  MOTION = 'motion',
+  CO2 = 'co2',
+  AMMONIA = 'ammonia',
+  GPS = 'gps',
+  RELAY = 'relay',
+  // Add more capabilities as needed
+}
