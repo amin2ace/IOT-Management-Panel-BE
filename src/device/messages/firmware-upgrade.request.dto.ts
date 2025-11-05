eb9cd54dbfc070feb45a7ebd7fe26088902b8e50
Amin Vahdani <amin.vahdani@payever.org>
2025-11-06 03:09:58 +0330

    feat: Implement data dto validation for firmware upgrade process

---
+++ src/device/messages/firmware-upgrade.request.dto.ts
@@ -0,0 +1,118 @@
+// src/device/dto/firmware-upgrade.dto.ts
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsOptional,
+  IsUrl,
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+} from 'class-validator';
+import { IsValidTimestampMillis } from 'src/config/decorator/timestamp-validation.decorator';
+
+export class FirmwareUpgradeDto {
+  @ApiProperty({
+    description: 'Unique identifier for the request',
+    example: 'req-12345',
+  })
+  @IsString()
+  @IsNotEmpty()
+  requestId: string;
+
+  @ApiProperty({
+    description: 'Numeric code representing the request type',
+    example: 101,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  requestCode: number; // Request Message Code
+
+  @ApiProperty({
+    description: 'Unique identifier of the device',
+    example: 'sensor-67890',
+  })
+  @IsString()
+  @IsNotEmpty()
+  deviceId: string; // Request from specific device
+
+  @ApiProperty({
+    description: 'Time of the request in epoch milli second',
+    example: '1762379573804',
+  })
+  @IsValidTimestampMillis() // 5min behind, 30sec ahead
+  @IsNotEmpty()
+  timestamp: number;
+
+  @ApiProperty({
+    description: 'Target firmware version for the device',
+    example: 'v1.2.3',
+  })
+  @IsString()
+  version: string;
+
+  @ApiProperty({
+    description: 'URL to download the firmware binary',
+    example: 'http://server.com/firmware/v1.2.3.bin',
+  })
+  @IsUrl({}, { message: 'Invalid URL format' })
+  url: string;
+
+  @ApiProperty({
+    description: 'Size of firmware binary file in KB',
+    example: 'http://server.com/firmware/v1.2.3.bin',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  size: number;
+
+  @ApiProperty({
+    description: 'Firmware binary file checksum in CRC32',
+    example: '3F4A9B2C',
+  })
+  @IsString()
+  @IsNotEmpty()
+  checksum: string;
+
+  @ApiProperty({
+    description: 'A base64 signature to validate request source',
+    example: 'U29tZVJhbmRvbUJhc2U2NFZhbHVl',
+  })
+  @IsString()
+  @IsNotEmpty()
+  signature: string;
+
+  @ApiProperty({
+    description: 'Optional release notes for this firmware version',
+    required: false,
+    example: 'Fixed sensor drift and improved connectivity.',
+  })
+  @IsOptional()
+  @IsString()
+  releaseNotes?: string;
+
+  @ApiProperty({
+    description: 'Whether the update should be applied immediately',
+    required: false,
+    example: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  forceUpdate?: boolean;
+}
+
+/**
+  Example:
+  {
+  "requestId": "fw-20251104-0004",
+  "requestCode": 104,
+  "deviceId": 'sensor-67890,
+  "timestamp": "1762379573804",
+  "version": "v1.3.0",
+  "url": "https://files.example.com/firmware/client-123/T1000/v1.3.0.bin?sig=...",
+  "size": 345678,
+  "checksum": "3F4A9B2C",
+  "signature": "U29tZVJhbmRvbUJhc2U2NFZhbHVl"
+  "releaseNotes": "Fixed sensor drift and improved connectivity."
+  "forceUpdate": false,
+}
+ */
---
+++ src/device/messages/firmware-upgrade.response.dto.ts
@@ -0,0 +1,66 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber, IsEnum } from 'class-validator';
+import { IsValidTimestampMillis } from 'src/config/decorator/timestamp-validation.decorator';
+
+export class DeviceResponseDto {
+  @ApiProperty({
+    description: 'Unique identifier for the response',
+    example: 'fw-20251104-status',
+  })
+  @IsString()
+  responsetId: string;
+
+  @ApiProperty({
+    description: 'Response code from the device or system',
+    example: '204',
+  })
+  @IsString()
+  responseCode: string;
+
+  @ApiProperty({
+    description: 'Unique identifier for the request',
+    example: 'fw-20251104-0004',
+  })
+  @IsString()
+  requestId: string;
+
+  @ApiProperty({
+    description: 'Identifier of the device',
+    example: 'sensor-67890',
+  })
+  @IsString()
+  deviceId: string;
+
+  @ApiProperty({
+    description: 'Timestamp in milliseconds',
+    example: '1762379573804',
+  })
+  @IsValidTimestampMillis()
+  timestamp: string;
+
+  @ApiProperty({
+    description: 'Current status of the device operation',
+  })
+  @IsString()
+  status: string;
+
+  @ApiProperty({
+    description: 'Progress percentage of the operation',
+    example: 12,
+  })
+  @IsNumber()
+  progress: number;
+}
+
+/*
+  Example:
+  {
+    responsetId: 'fw-20251104-status',
+    responseCode: '204',
+    requestId: 'fw-20251104-0004',
+    deviceId: 'sensor-67890',
+    timestamp: '1762379573804',
+    status: 'DOWNLOADING',
+    progress: 12,
+};
+  **/
