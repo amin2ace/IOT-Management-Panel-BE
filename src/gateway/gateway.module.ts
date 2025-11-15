ee819b9ed6da17cdefdd13de0c1bd07d6923c858
Amin Vahdani <amin.vahdani@payever.org>
2025-11-16 01:42:42 +0330

    add discovery request subscriptions to ws gateway

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -3,9 +3,14 @@ import { MqttGatewayService } from './gateway.service';
 import { MqttClientModule } from '../mqtt-client/mqtt-client.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import MessageIncoming from './repository/message-incoming.entity';
+import { DeviceModule } from '@/device/device.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MessageIncoming]), MqttClientModule],
+  imports: [
+    TypeOrmModule.forFeature([MessageIncoming]),
+    MqttClientModule,
+    DeviceModule,
+  ],
   providers: [MqttGatewayService],
   exports: [MqttGatewayService],
 })
