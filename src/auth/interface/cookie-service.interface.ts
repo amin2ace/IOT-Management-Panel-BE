a443e40d5ec7c6e7c83826d7a81dc6f27435e4cc
amin2ace <av.amin73@gmail.com>
2026-08-08 23:28:39 +0330

    refactor: enhance ICookieService interface documentation for clarity and consistency

--- src/auth/interface/cookie-service.interface.ts
+++ src/auth/interface/cookie-service.interface.ts
@@ -2,19 +2,41 @@ import { Request, Response } from 'express';
 import { TokenType } from 'src/config/enum/token-type.enum';
 
 export interface ICookieService {
+  /**
+   * Set token parameters in cookie
+   * @param res
+   * @param tokenType
+   * @param token
+   */
   setTokensCookie(
     res: Response,
     tokenType: TokenType,
     token: string,
   ): Promise<string>;
-
+  /**
+   * Retrieve token from cookie
+   * @param req
+   * @param tokenType
+   */
   getRefreshTokenFromCookie(
     req: Request,
     tokenType: TokenType,
   ): Promise<{ refreshToken: string }>;
-
+  /**
+   * Clear all cookie entries
+   * @param res
+   * @param tokenType
+   */
   clearCookie(res: Response, tokenType: TokenType): Promise<string>;
-
+  /**
+   * Set session id in cookie
+   * @param res
+   * @param sessionId
+   */
   setSessionCookie(res: Response, sessionId: string): Promise<void>;
+  /**
+   * Clear session id from cookie
+   * @param res
+   */
   clearSessionCookie(res: Response): Promise<void>;
 }
