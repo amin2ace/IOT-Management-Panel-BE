7ab4f266801aadb225a600b9666a24ad0a59b7ea
amin2ace <av.amin73@gmail.com>
2026-08-10 23:07:30 +0330

    refactor: update environment variable handling for config module

--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -2,6 +2,6 @@ export * from './change-password.dto';
 export * from './forget-password.dto';
 export * from './login.dto';
 export * from './reset-password.dto';
-export * from './user-response.dto';
+export * from './auth-response.dto';
 export * from './signup.dto';
 export * from './token-input.dto';
--- src/config/config-module.ts
+++ src/config/config-module.ts
@@ -1,10 +1,10 @@
 import { ConfigModuleOptions } from '@nestjs/config';
 import configValidationSchema from './joi-validation-schema.config';
+import { isProductionEnvironment } from '@/common/utils/environment.util';
 
-const envFilePath =
-  process.env.NODE_ENV === 'production'
-    ? ['.env.production', '.env']
-    : ['.env.development', '.env'];
+const envFilePath = isProductionEnvironment()
+  ? ['.env.production', '.env']
+  : ['.env.development', '.env'];
 
 const configModuleOptions: ConfigModuleOptions = {
   isGlobal: true,
